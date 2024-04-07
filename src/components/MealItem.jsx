bf0bba977f3c60ef678c7a6a21d89b1dea8c854a
DuckFunny2k <lamducfpt2004@gmail.com>
2024-04-08 00:46:29 +0700

    updateUi

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,21 @@
+import { currentFormatter } from "../util/Formatting";
+import Button from "./ui/Button.jsx";
+export default function MealItem({ meal }) {
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">
+            {currentFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button>Add to cart</Button>
+        </p>
+      </article>
+    </li>
+  );
+}
---
+++ src/util/Formatting.jsx
@@ -0,0 +1,4 @@
+export const currentFormatter = new Intl.NumberFormat("vi-VN", {
+  style: "currency",
+  currency: "VND",
+});
