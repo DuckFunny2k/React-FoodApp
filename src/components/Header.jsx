bfec6bd2374834eb74ba368404bf27638a5d3376
DuckFunny2k <lamducfpt2004@gmail.com>
2024-04-09 01:25:55 +0700

    updateCart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 import logo from "../assets/logo.jpg";
 import Button from "./ui/Button.jsx";
+import { useContext } from "react";
+import cartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Header() {
+  const cartCtx = useContext(cartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+  const totalQuantity = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.quantity;
+  }, 0);
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
   return (
     <header id="main-header" className="w-full">
       <div id="title">
@@ -9,7 +20,9 @@ export default function Header() {
         <h1>Food App</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalQuantity})
+        </Button>
       </nav>
     </header>
   );
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,6 +1,12 @@
 import { currentFormatter } from "../util/Formatting";
 import Button from "./ui/Button.jsx";
+import { useContext } from "react";
+import CartContext from "../store/CartContext.jsx";
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
   return (
     <li className="meal-item">
       <article>
@@ -13,7 +19,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to cart</Button>
         </p>
       </article>
     </li>
